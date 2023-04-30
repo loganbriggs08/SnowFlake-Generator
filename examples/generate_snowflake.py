f8c2d947fe66a0a2748428e342eed4a486b17286
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-04-30 21:18:29 +0100

    Fetch snowflake from dict

--- examples/generate_snowflake.py
+++ examples/generate_snowflake.py
@@ -3,4 +3,4 @@ from snowflake import Snowflake
 Snowflake_Client: object = Snowflake(node_id=1, worker_id=1)
 Random_Snowflake: int = Snowflake_Client.generate()
 
-print(Random_Snowflake)
+print(Random_Snowflake["snowflake"])
