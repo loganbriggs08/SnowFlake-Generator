262db279907441593c538a9a05e44b48cb4fb7dc
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-04-30 21:17:46 +0100

    Convert snowflake to binary to then fetch information

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from snowflake import Snowflake
 
 Snowflake_Client: object = Snowflake(node_id=1, worker_id=1)
-Random_Snowflake: int = Snowflake_Client.generate()
+Random_Snowflake: dict[str] = Snowflake_Client.generate()
+Our_Snowflake: int = Random_Snowflake["snowflake"]
+Snowflake_Binary: int = Snowflake_Client.snowflake_to_binary(Our_Snowflake)
 
-print(Random_Snowflake)
-
-# print(Snowflake_Client.fetch_random_number(Random_Snowflake))
-# print(Snowflake_Client.fetch_worker_id(Random_Snowflake))
-# print(Snowflake_Client.fetch_node_id(Random_Snowflake))
+print(Our_Snowflake)
+print(Snowflake_Client.fetch_random_number(Snowflake_Binary))
+print(Snowflake_Client.fetch_worker_id(Snowflake_Binary))
+print(Snowflake_Client.fetch_node_id(Snowflake_Binary))
