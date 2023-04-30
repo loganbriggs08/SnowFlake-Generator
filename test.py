e86c53543025e48e8fa48114db01fea5916189be
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-04-30 20:58:59 +0100

    Add some basic tests to test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from snowflake import Snowflake
 
 Snowflake_Client: object = Snowflake(node_id=1, worker_id=1)
-print(Snowflake_Client.generate())
+Random_Snowflake: int = Snowflake_Client.generate()
+
+print(Random_Snowflake)
+print(Snowflake_Client.fetch_random_number(Random_Snowflake))
+print(Snowflake_Client.fetch_worker_id(Random_Snowflake))
+print(Snowflake_Client.fetch_node_id(Random_Snowflake))
