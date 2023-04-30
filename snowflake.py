aa15bfee9d48de2b3381ffdcdab68777aff05b91
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-04-30 21:08:21 +0100

    Functions now turn the snowflake_id into binary then return the correct part

--- snowflake.py
+++ snowflake.py
@@ -8,19 +8,27 @@ class Snowflake:
         self.worker_id: int = worker_id
         
     @staticmethod
-    def fetch_random_number(snowflake_binary: int) -> int:
+    def fetch_random_number(snowflake_id: int) -> int:
+        snowflake_binary: int = bin(snowflake_id)[2:].zfill(12)
+        
         return int(snowflake_binary[52:], 2)
     
     @staticmethod
-    def fetch_worker_id(snowflake_binary: int) -> int:
+    def fetch_worker_id(snowflake_id: int) -> int:
+        snowflake_binary: int = bin(snowflake_id)[2:].zfill(5)
+        
         return int(snowflake_binary[47:52], 2)
     
     @staticmethod
-    def fetch_node_id(snowflake_binary: int) -> int:
+    def fetch_node_id(snowflake_id: int) -> int:
+        snowflake_binary: int = bin(snowflake_id)[2:].zfill(5)
+        
         return int(snowflake_binary[42:47], 2)
     
     @staticmethod
     def fetch_time_since_epoch(snowflake_binary: int) -> int:
+        snowflake_binary: int = bin(snowflake_binary)[2:].zfill(41)
+        
         return int(snowflake_binary[1:42], 2)
       
     def check_node_and_worker(self) -> int:
@@ -41,7 +49,7 @@ class Snowflake:
         
         snowflake_binary: str = self._generate_snowflake()
         
-        return snowflake_binary
+        return int(snowflake_binary, 2)
     
     def _generate_snowflake(self) -> str:
         """Generate a random snowflake.
@@ -58,9 +66,7 @@ class Snowflake:
         node_id_binary: str = bin(node_id)[2:].zfill(5)
         worker_id_binary: str = bin(worker_id)[2:].zfill(5)
         random_12_bit_number_binary: str = bin(random_12_bit_number)[2:].zfill(12)
-        
-        print(random_12_bit_number)
-        
+                
         snowflake_binary: str = f"0{time_since_epoch_binary}{node_id_binary}{worker_id_binary}{random_12_bit_number_binary}"
         
         return snowflake_binary
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ Snowflake_Client: object = Snowflake(node_id=1, worker_id=1)
 Random_Snowflake: int = Snowflake_Client.generate()
 
 print(Random_Snowflake)
-print(Snowflake_Client.fetch_random_number(Random_Snowflake))
-print(Snowflake_Client.fetch_worker_id(Random_Snowflake))
-print(Snowflake_Client.fetch_node_id(Random_Snowflake))
+
+# print(Snowflake_Client.fetch_random_number(Random_Snowflake))
+# print(Snowflake_Client.fetch_worker_id(Random_Snowflake))
+# print(Snowflake_Client.fetch_node_id(Random_Snowflake))
